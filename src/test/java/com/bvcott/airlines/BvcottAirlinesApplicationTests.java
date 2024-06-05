b52898c42dedf05f28221d26668ae9697d86d9c9
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2024-06-05 10:37:13 +0100

    first commit

---
+++ src/test/java/com/bvcott/airlines/BvcottAirlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvcott.airlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvcottAirlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
