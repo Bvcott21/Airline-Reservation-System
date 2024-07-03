8ceedbc6742906615d31098956fc11fb4ce8f23c
edgar.afonso <edgar.afonso@fdmgroup.com>
2024-07-03 10:13:25 +0100

    Added findAll, findById, Update and Delete functionality to Airport's Service Layer.

--- backend/src/main/java/com/bvcott/airlines/service/AirportService.java
+++ backend/src/main/java/com/bvcott/airlines/service/AirportService.java
@@ -1,12 +1,15 @@
 package com.bvcott.airlines.service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import com.bvcott.airlines.exception.AirportAlreadyExistsException;
+import com.bvcott.airlines.exception.AirportNotFoundException;
 import com.bvcott.airlines.model.Airport;
 import com.bvcott.airlines.repository.AirportRepository;
 
@@ -40,4 +43,67 @@ public class AirportService {
         }
     }
 
+    public List<Airport> retrieveAll() {
+        log.debug("retrieveAll method called.");
+        return airportRepo.findAll();
+    }
+
+    public Airport retrieveById(UUID id) {
+        log.debug("retrieveById method called with ID: " + id);
+        return airportRepo
+            .findById(id)
+            .orElseThrow(() -> new AirportNotFoundException("Airport not found with ID: " + id));
+    }
+
+    public void deleteById(UUID id) {
+        log.debug("deleteById method called, ID received: {}", id);
+        Optional<Airport> airportFound = airportRepo.findById(id);
+
+        if(airportFound.isPresent()) {
+            airportRepo.deleteById(id);
+        } else {
+            log.warn("Airport with ID: {} - NOT FOUND", id);
+            throw new AirportNotFoundException("Can't delete, airport not found with ID: " + id);
+        }
+    }
+
+    public Airport updateAirport(UUID id, Airport airport) {
+        log.debug("updateAirport method called with ID: {}, and object: {}", id, airport);
+        
+        if(airport.getName().isEmpty() || airport.getCode().isEmpty() || airport.getLocation().isEmpty()) {
+            log.warn("One or more of the following fields are empty: name, code, location");
+            throw new DataIntegrityViolationException("Can't update, one or more of the following fields are empty: name, code, location.");
+        }
+
+        Optional<Airport> airportFound = airportRepo.findById(id);
+
+        if(!airportFound.isPresent()) {
+            log.warn("Airport not found with ID: {}", id);
+            throw new AirportNotFoundException("Airport not found with ID: " + id);
+        }
+
+        Airport existingAirport = airportFound.get();
+
+        Optional<Airport> nameMatcher = airportRepo.findByName(airport.getName());
+        Optional<Airport> codeMatcher = airportRepo.findByCode(airport.getCode());
+
+        if(nameMatcher.isPresent()) {
+            log.warn("Airport with name: {} already exists", airport.getName());
+            throw new AirportAlreadyExistsException("Airport already exists with name: " + airport.getName());
+        }
+
+        if(codeMatcher.isPresent()) {
+            log.warn("Airport with code: {} already exists", airport.getCode());
+            throw new AirportAlreadyExistsException("Airport already exists with code: " + airport.getCode());
+        }
+
+        existingAirport.setName(airport.getName());
+        existingAirport.setCode(airport.getCode());
+        existingAirport.setLocation(airport.getLocation());
+
+        Airport updatedAirport = airportRepo.save(existingAirport);
+        
+        log.debug("Airport updated successfully, updated object: {}", updatedAirport);
+        return updatedAirport;
+    }
 }
--- backend/src/test/java/com/bvcott/airlines/service/AirportServiceTest.java
+++ backend/src/test/java/com/bvcott/airlines/service/AirportServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import com.bvcott.airlines.exception.AirportAlreadyExistsException;
+import com.bvcott.airlines.exception.AirportNotFoundException;
 import com.bvcott.airlines.model.Airport;
 import com.bvcott.airlines.repository.AirportRepository;
 
@@ -95,6 +97,173 @@ public class AirportServiceTest {
         
         verify(airportRepo).save(airport);
         assertEquals(airport, createdAirport);
+    }
+
+    @Test
+    void retrieveAll_callsAirportRepository_findAllMethod_whenCalled() {
+        airportService.retrieveAll();
+        verify(airportRepo).findAll();
+    }
+
+    @Test
+    void test_retrieveByIdCalls_airportRepository_findById_whenCalled() {
+        UUID id = UUID.randomUUID();
+        Airport airport = new Airport("LUT", "Luton", "UK");
+
+        when(airportRepo.findById(id)).thenReturn(Optional.of(airport));
+        airportService.retrieveById(id);
+
+        verify(airportRepo).findById(id);
+    }
+
+    @Test
+    void test_retrieveById_throwsAirportNotFoundException_ifNoAirportFound() {
+        UUID id = UUID.randomUUID();
+        assertThrows(AirportNotFoundException.class, () -> airportService.retrieveById(id));
+    }
+
+    @Test
+    void test_retrieveById_returnsCorrectAirportObject() {
+        UUID id = UUID.randomUUID();
+        Airport airport = new Airport("LUT", "Luton", "UK");
+
+        when(airportRepo.findById(id)).thenReturn(Optional.of(airport));
+        Airport savedAirport = airportService.retrieveById(id);
+
+        assertEquals(airport, savedAirport);
+        verify(airportRepo).findById(id);
+    }
+
+    @Test
+    void test_deleteById_callsAirportRepo_findById() {
+        UUID id = UUID.randomUUID();
+        Airport airport = new Airport("LUT", "Luton", "UK");
+
+        when(airportRepo.findById(id)).thenReturn(Optional.of(airport));
+
+        airportService.deleteById(id);
+        verify(airportRepo).findById(id);
+    }
+
+    @Test
+    void test_deleteById_throwsAirportNotFoundException_ifAirportNotFound() {
+        UUID id = UUID.randomUUID();
+        when(airportRepo.findById(id)).thenReturn(Optional.empty());
+        assertThrows(AirportNotFoundException.class, () -> airportService.deleteById(id));
+    }
+
+    @Test
+    void test_deleteById_calls_airportRepo_deleteById_whenAirportIsFound() {
+        UUID id = UUID.randomUUID();
+        Airport airport = new Airport("LUT", "Luton", "UK");
+
+        when(airportRepo.findById(id)).thenReturn(Optional.of(airport));
+
+        airportService.deleteById(id);
+        verify(airportRepo).deleteById(id);
+    }
+
+    @Test
+    void test_updateAirport_callsAirportRepo_findById_whenCalled() {
+        UUID id = UUID.randomUUID();
+        Airport airport = new Airport("LUT", "Luton", "UK");
+
+        when(airportRepo.findById(id)).thenReturn(Optional.of(airport));
+
+        airportService.updateAirport(id, airport);
+
+        verify(airportRepo).findById(id);
+    }
+
+    @Test
+    void test_updateAirport_throwsAirportNotFoundException_ifAirportNotFound() {
+        UUID id = UUID.randomUUID();
+        Airport airport = new Airport("LUT", "Luton", "UK");
+
+        assertThrows(AirportNotFoundException.class, () ->  airportService.updateAirport(id, airport));
+    }
+
+    @Test
+    void test_updateAirport_throwsDataIntegrityViolationException_ifNameIsEmpty() {
+        Airport airport = new Airport("LUT", "", "UK");
+        assertThrows(DataIntegrityViolationException.class, () -> airportService.updateAirport(UUID.randomUUID(), airport));
+    }
+
+    @Test
+    void test_updateAirport_throwsDataIntegrityViolationException_ifCodeIsEmpty() {
+        Airport airport = new Airport("", "Luton", "UK");
+        assertThrows(DataIntegrityViolationException.class, () -> airportService.updateAirport(UUID.randomUUID(), airport));
+    }
+
+    @Test
+    void test_updateAirport_throwsDataIntegrityViolationException_ifLocationIsEmpty() {
+        Airport airport = new Airport("LUT", "Luton", "");
+        assertThrows(DataIntegrityViolationException.class, () -> airportService.updateAirport(UUID.randomUUID(), airport));
+    }
+    
+    @Test
+    void test_updateAirport_callsAirportRepo_findByName() {
+        UUID id = UUID.randomUUID();
+        Airport airport = new Airport("LUT", "Luton", "UK");
+
+        when(airportRepo.findById(id)).thenReturn(Optional.of(airport));
+
+        airportService.updateAirport(id, airport);
+        
+        verify(airportRepo).findByName(airport.getName());
+    }
+
+    @Test
+    void test_updateAirport_callsAirportRepo_findByCode() {
+        UUID id = UUID.randomUUID();
+        Airport airport = new Airport("LUT", "Luton", "UK");
+
+        when(airportRepo.findById(id)).thenReturn(Optional.of(airport));
+
+        airportService.updateAirport(id, airport);
+
+        verify(airportRepo).findByCode(airport.getCode());
+    }
+
+    @Test
+    void test_updateAirport_throwsAirportAlreadyExistsException_ifNameExsists() {
+        UUID id = UUID.randomUUID();
+        Airport airport = new Airport("LUT", "Luton", "UK");
+
+        when(airportRepo.findById(id)).thenReturn(Optional.of(airport));
+        when(airportRepo.findByName(airport.getName())).thenReturn(Optional.of(airport));
+
+        assertThrows(AirportAlreadyExistsException.class, () -> airportService.updateAirport(id, airport));
+    }
+
+    @Test
+    void test_updateAirport_throwsAirportAlreadyExistsException_ifCodeExsists() {
+        UUID id = UUID.randomUUID();
+        Airport airport = new Airport("LUT", "Luton", "UK");
+
+        when(airportRepo.findById(id)).thenReturn(Optional.of(airport));
+        when(airportRepo.findByCode(airport.getCode())).thenReturn(Optional.of(airport));
+
+        assertThrows(AirportAlreadyExistsException.class, () -> airportService.updateAirport(id, airport));
+    }
+
+    @Test
+    void test_updateAirport_successfullyUpdatesAirport() {
+        UUID id = UUID.randomUUID();
+        Airport existingAirport = new Airport("LUT", "Luton", "UK");
+        Airport updatedAirport = new Airport("NEW", "new-airport", "NEW");
+
+        when(airportRepo.findById(id)).thenReturn(Optional.of(existingAirport));
+        when(airportRepo.findByCode(existingAirport.getCode())).thenReturn(Optional.empty());
+        when(airportRepo.findByName(existingAirport.getName())).thenReturn(Optional.empty());
+        when(airportRepo.save(existingAirport)).thenReturn(updatedAirport);
+
+        Airport result = airportService.updateAirport(id, updatedAirport);
+
+        assertEquals(updatedAirport.getName(), result.getName());
+        assertEquals(updatedAirport.getCode(), result.getCode());
+        assertEquals(updatedAirport.getLocation(), result.getLocation());
+        verify(airportRepo).save(existingAirport);
     }
 
 }
